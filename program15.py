af11ee1c4904e3784238cf5d9e465d0975b1a45a
asthajain23 <asthajain2308@gmail.com>
2017-06-09 15:46:17 +0530

    Create program15.py

---
+++ program15.py
@@ -0,0 +1,12 @@
+# Write a Python program to assess if a file is closed or not
+fo=open("file1.txt","w+")
+fo = open("file1.txt","r")
+fo.close()
+print(fo.closed)
+
+
+
+'''Output
+python program15.py
+true
+'''
